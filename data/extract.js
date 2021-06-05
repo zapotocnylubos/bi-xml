7d177ff0a06fc4074bab78fcf4e2135a63b0c9d9
Lubos Zapotocny <zapotocnylubos@gmail.com>
2021-06-05 08:27:31 +0200

    Add extract/filter script for raw dataset

---
+++ data/extract.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const fs = require('fs');
+
+const data = JSON.parse(fs.readFileSync('factbook.json'));
+
+const areas = ['czechia', 'italy', 'greece', 'egypt'];
+
+let output = {
+    countries: areas.reduce(function (countries, area) {
+        countries[area] = data.countries[area];
+        return countries
+    }, {}) // Pick only pre-defined countries
+};
+
+const output_filename = 'factbook_' + areas.join('_') + '.json';
+const output_data = JSON.stringify(output, null, '  ');
+
+fs.writeFileSync(output_filename, output_data); // Save reduced dataset to file
